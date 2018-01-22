49b86e2899e36f6d5c5a743d5ea45662c69cdcca
ANDES <andresmontealegregiraldo@gmail.com>
2018-01-22 16:48:07 -0500

    Operaciones Basicas

--- js/app.js
+++ js/app.js
@@ -1,47 +1,59 @@
 var calculadora= {
     init: function (){
-    //  this.reducirTecla(),
-    //this.mostrarPantalla(),
+    this.reducirTecla(),
+    this.mostrarPantalla("0"),
     //  this.teclaIgual(),
-    //this.asignarNum1()
+    this.recibirTecla()
     this.interno,
     this.borrarPantalla()
+    //this.obtenerTecla()
     },
     interno:{
+        numeroT:0,
         numeroA:0,
         numeroB:0,
-        intent:0,
+        intento:0,
         operador:"",
-        resultado:""
+        resultado:"0",
+        signo:""
     },
-    cambiarNum: function(a,b,o){
-      this.interno.numeroA=a,
-      this.interno.numeroB=b,
-      this.interno.operador=o
-    },
+
     teclaIgual: function(){
+      var self=this
+
+      if(self.interno.intento=2){
+        self.interno.numeroB=self.interno.numeroT
+      }
+
       var o=this.interno.operador
+      var a=Number(this.interno.numeroA)
+      var b=Number(this.interno.numeroB)
+      var resultado=0
       switch (o) {
-        case "mas":
-          var resultado= this.interno.numeroA +this.interno.numeroB
-          break;
+          case "mas":
+            resultado= a + b
+            break;
 
-        case "menos":
-            var resultado= this.interno.numeroA  - this.interno.numeroB
-          break;
+          case "menos":
+              resultado= a  - b
+            break;
 
-        case "por":
-              var resultado= this.interno.numeroA * this.interno.numeroB
-          break;
+          case "por":
+                resultado= a * b
+            break;
 
-        case "dividido":
-              var resultado= this.interno.numeroA + this.interno.numeroB
-          break;
-        default:
-          var resultado=0
-          break;
-        }
-      this.mostrarPantalla(resultado)
+          case "dividido":
+                resultado= a / b
+            break;
+
+            default:
+          }
+        this.interno.numeroA=0
+        this.interno.numeroB=0
+        this.interno.numeroT=resultado
+        self.interno.intento=0
+        resultado=resultado.toString()
+        this.mostrarPantalla(resultado)
 },
 
     borrarPantalla: function(){
@@ -49,36 +61,117 @@ var calculadora= {
         var p=document.getElementById('display')
         var teclaOn    = document.getElementById('on')
         teclaOn.addEventListener('click', function(){
-        p.innerHTML='0'
+        p.innerHTML=  self.interno.resultado
         // limpiar Variables
-        this.interno.numeroA=100
+        self.interno.numeroA=0
+        self.interno.numeroB=0
+        self.interno.numeroT=0
+        self.interno.intento=0
         })
     },
 
       recibirOperador: function(o){
         this.interno.operador=o
+        console.log("Operador"+this.interno.operador)
         var p=document.getElementById('display')
             p.innerHTML=''
       },
-      mostrarPantalla: function(obj){
-        //Validar los 8 caracteres
+      validarTam: function (c){
         var self=this
-          var p=document.getElementById('display')
-          p.innerHTML=obj
+        self.mostrarPantalla(c)
       },
-      asignarNum1: function (){
+      mostrarPantalla: function(obj){ //Recibe string
+        var p=document.getElementById('display')
+        p.innerHTML=obj
+      },
+      limpiarPantalla:  function(a,b){
+        var self=this
+        self.interno.operador=a
+        if(b==0){self.interno.numeroA=self.interno.numeroT}
+        if(b==2){self.interno.numeroB=self.interno.numeroT}
+        self.interno.intento=2
+        self.interno.numeroT=0
+        var p=document.getElementById('display')
+        p.innerHTML=""
+      },
+      recibirTecla: function (){
+        var self  = this
+        var teclas = document.getElementsByClassName('tecla')
+        for (var i=0;i<teclas.length;i++) {
+            teclas[i].addEventListener('click', function(ev){
+              this.id
+              self.obtenerTecla(this.id,self.interno.intento)
+            })
+        }
+      },
+      obtenerTecla: function(a,b){
+        var self=this
+        var numRecibido
+
+         if(a=="mas"|| a=="menos" || a=="por" || a=="dividido"){
+           self.limpiarPantalla(a,self.interno.intento)
+         } else if(a=="igual"){
+            self.teclaIgual()
+         } else if (a=="signo"){
+      //     self.teclaIgual()
+            console.log("signo")
+         }else if (a=="raiz"){
+              console.log("raiz")
+         } else {
+          self.interno.numeroT=self.interno.numeroT+a
+          self.interno.numeroT= self.validarCero(self.interno.numeroT)
+          console.log(self.interno.numeroT)
+          self.validarTam(self.interno.numeroT)
+           }
       },
 
+
+      validarCero: function(c){
+        if(c[0]=="0"){
+          c=c.substring(1,c.length)
+          console.log (c)
+        }
+        return c
+      },
       reducirTecla: function(){
       var self  = this
+      var wmin=5
+      var hmin=2
       var teclas    = document.getElementsByClassName('tecla')
       for (var i=0;i<teclas.length;i++) {
             teclas[i].addEventListener('mousedown', function(ev){
-                    var teclaPress=this
-                    console.log(teclaPress.id)
+
+
+                    var tPress=this
+                  if(tPress.id!="mas"){
+                    var wOrigin=tPress.clientWidth
+                    var hOrigin=tPress.clientHeight
+                    tPress.style.width=(wOrigin-wmin)+"px"
+                    tPress.style.height=(hOrigin-hmin)+"px"
+                  } else {
+                    var wOrigin=tPress.clientWidth
+                    var hOrigin=tPress.clientHeight
+                    tPress.style.width=(wOrigin-2)+"px"
+                    tPress.style.height=(hOrigin-2)+"px"
+                  }
+
+
                   })
             teclas[i].addEventListener('mouseup', function(ev){
-                      var teclaPress=this
+                      var tPress=this
+                      if(tPress.id!="mas"){
+                          var wOrigin=tPress.clientWidth
+                          var hOrigin=tPress.clientHeight
+                          tPress.style.width=(wOrigin+wmin)+"px"
+                          tPress.style.height=(hOrigin+hmin)+"px"
+                    } else {
+                      var wOrigin=tPress.clientWidth
+                      var hOrigin=tPress.clientHeight
+                      tPress.style.width=(wOrigin+2)+"px"
+                      tPress.style.height=(hOrigin+2)+"px"
+
+
+                    }
                     })
                 }
       }
